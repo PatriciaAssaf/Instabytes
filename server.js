52ba2225cf1fd2b89cdf1e3dc09f0269613d3855
Patricia Assaf <assafpati@gmail.com>
2024-11-20 12:20:27 -0300

    terceiro dia da  imersão

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const posts = [
 ]; 
 
 const app = express ();
-app.use (express.json());
 
 app.listen(3000, () => {
     console.log ("Servidor escutando...");
@@ -17,13 +16,4 @@ app.get("/posts", (req, res) => {
     res.status(200).json(posts);
 });
 
-function buscarPostPorID (id) {
-    return posts.findIndex ((post) => {
-        return post.id === Number (id)
-    })
-}
 
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorID (req.params.id)
-    res.status(200).json(posts[index]);
-});
