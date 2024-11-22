3d61398495fad562c59323c06d038840af27a536
Patricia Assaf <assafpati@gmail.com>
2024-11-22 18:15:16 -0300

    quinto dia da imersão

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,5 +1,6 @@
-import {getTodosPosts, criarPost} from "../models/postsModel.js";
+import {getTodosPosts, criarPost, atualizarPost} from "../models/postsModel.js";
 import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiservice.js"
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
@@ -34,3 +35,24 @@ export async function uploadImagem(req, res) {
         res.status(500).json({"Erro":"Falha na requisição"})
     }
 }
+
+export async function atualizarNovoPost(req, res) {
+        const id = req.params.id;
+        const urlImagem = `http://localhost:3000/${id}.png`
+        
+        try {
+            const imageBuffer = fs.readFileSync (`uploads/${id}.png`)
+            const descricao = await gerarDescricaoComGemini (imageBuffer)
+
+            const post = {
+                imgUrl: urlImagem,
+                descricao: descricao,
+                alt: req.body.alt
+            }
+            const postCriado = await atualizarPost(id, post);
+            res.status(200).json(postCriado);
+        } catch(erro) {
+            console.error(erro.message);
+            res.status(500).json({"Erro":"Falha na requisição"})
+        }
+    }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,7 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsControllers.js";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsControllers.js";
+import cors from "cors"
 
+const corsOptions = {
+    origin: "http://loclhost:8000",
+    optionsSuccessStatus: 200
+}
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
@@ -15,10 +20,14 @@ const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
     app.use (express.json());
+    app.use (cors(corsOptions))
 
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPost);
     app.post("/upload", upload.single("imagem"), uploadImagem)
-}
+
+    app.put("/upload/id", atualizarNovoPost)
+};
+
 
 export default routes;
